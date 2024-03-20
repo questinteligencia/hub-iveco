34af93242585be26c61be3eb39e8fefbaf8b0be7
lfrubin <lf_rubin@hotmail.com>
2024-03-20 18:42:00 -0300

    change database server IP address

--- src/config/database.ts
+++ src/config/database.ts
@@ -1,6 +1,6 @@
 import {Sequelize} from "sequelize";
 
-const URL = process.env.MYSQL_IVECO_HOST || '107.21.172.136';
+const URL = process.env.MYSQL_IVECO_HOST || '54.145.224.78';
 const USERNAME = process.env.MYSQL_IVECO_USERNAME || 'hubiveco';
 const PASSWORD = process.env.MYSQL_IVECO_PASSWORD || 'hub1vec0#2023';
 const DATABASE = process.env.MYSQL_IVECO_DATABASE || 'hubivecodb';
@@ -8,7 +8,7 @@ const PORT = process.env.MYSQL_IVECO_PORT || '3010';
 
 const connection = new Sequelize({
     dialect: 'mysql',
-    // host: '107.21.172.136',
+    // host: '54.145.224.78',
     host: URL,   
     // port: 3010,
     port: parseInt(PORT),
